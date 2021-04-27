2cdafc0ec6117a567900185ebab7f2c9102a563f
Deep Choudhery <decho@microsoft.com>
2021-04-26 23:48:57 -0700

    new blazorwasm hosted

---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_blazor_wasm_hosted_b2c.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
